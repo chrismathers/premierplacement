47f4091fe581411881c296eec9b1847562eff58c
chris mathers <xee@xemedia.com>
2018-08-22 15:42:19 -0400

    Added bootstrap nav stuff

--- wordpress/wp-content/themes/premier/nav.php
+++
@@ -1,9 +0,0 @@
-<nav>
-	<ul>
-		<li id="homelink"><a href="/">Home</a></li>
-		<li><a href="/about-us">About Us</a></li>
-		<li><a href="/resume-guide">Resume Guide</a></li>
-		<li><a href="/contact-us">Contact Us</a></li>
-		<li><a href="/interview-guide">Interview Guide</a></li>
-	</ul>
-</nav>
--- wordpress/wp-content/themes/premier/searchform.php
+++
@@ -1,8 +0,0 @@
-<form action="<?php bloginfo('siteurl'); ?>" id="searchform" method="get">
-	<div class="search-input-div">
-	<input type="text" placeholder="Search the blogs …" name="s" id="s" />
-	<a href="javascript:void(0)">
-	  <input type="submit" id="searchsubmit" value="GO" />
-	</a>
-	</div>
-</form>
